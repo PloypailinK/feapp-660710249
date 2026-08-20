879b0ebb210d40d64f036ada8d1179538e64ee79
PloypailinK <kwanwipithakran_p@su.ac.th>
2026-08-20 05:09:51 +0000

    First react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,28 +4,36 @@ import MovieList from "./components/MovieList";
 import Footer from "./components/Footer";
 import Greeting from "./components/Greeting";
 import Card from "./components/Card";
+import ProfileCard from "./components/ProfileCard";
+import './App.css';
 
-
-const students = [
-  { id: 1, name: 'ฝน',   year: 3 },
-  { id: 2, name: 'เต้ย', year: 2 },
-  { id: 3, name: 'มายด์', year: 4 },
+const members = [
+  { id: 1, name: 'สมหญิง ใจดี', nickname: 'ฝน',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาเขียว', 'แมว'] },
+ { id: 2, name: 'วริศราภรณ์ กันชัยต๊ะ', nickname: 'ต้นน้ำ',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['กาแฟ', 'แมว'] },
+    { id: 3, name: 'โอปอ โอปอ', nickname: 'โอปอ',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาไทย', 'หมา'] },
 ];
+
 
 function App() {
   return (
-    <div>
-      <Card title="ประกาศ">
-  <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
-  <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
-</Card>
-      <h1>รายชื่อนักศึกษา</h1>
-      {students.map((s) => (
-        <Greeting key={s.id} name={s.name} year={s.year} />
-      ))}
+    <div className="container">
+      <h1>สมาชิกกลุ่มของเรา</h1>
+      <div className="card-row">
+        {members.map((m) => (
+          <ProfileCard
+            key={m.id}
+            name={m.name}
+            nickname={m.nickname}
+            major={m.major}
+            favorites={m.favorites}
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
 
 export default App;
