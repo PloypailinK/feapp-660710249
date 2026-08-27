b2263cbb2552b11470c955360d6b0060c0da75ec
PloypailinK <kwanwipithakran_p@su.ac.th>
2026-08-27 05:27:40 +0000

    Movie Project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,26 @@
-import React from "react";
-import Header from "./components/Header";
-import MovieList from "./components/MovieList";
-import Footer from "./components/Footer";
-import Greeting from "./components/Greeting";
-import Card from "./components/Card";
-import ProfileCard from "./components/ProfileCard";
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import MovieDetail from './pages/MovieDetail';
 
-const members = [
-  { id: 1, name: 'สมหญิง ใจดี', nickname: 'ฝน',
-    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาเขียว', 'แมว'] },
- { id: 2, name: 'วริศราภรณ์ กันชัยต๊ะ', nickname: 'ต้นน้ำ',
-    major: 'เทคโนโลยีสารสนเทศ', favorites: ['กาแฟ', 'แมว'] },
-    { id: 3, name: 'โอปอ โอปอ', nickname: 'โอปอ',
-    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาไทย', 'หมา'] },
-];
 
 
 function App() {
   return (
-    <div className="container">
-      <h1>สมาชิกกลุ่มของเรา</h1>
-      <div className="card-row">
-        {members.map((m) => (
-          <ProfileCard
-            key={m.id}
-            name={m.name}
-            nickname={m.nickname}
-            major={m.major}
-            favorites={m.favorites}
-          />
-        ))}
-      </div>
+    <div className="flex min-h-screen flex-col bg-slate-50">
+      <Navbar />                {/* ← เห็นทุกหน้า */}
+      <main className="flex-1">
+        <Routes>                {/* ← เฉพาะตรงนี้ที่สลับตาม URL */}
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/movies/:id" element={<MovieDetail />} />
+        </Routes>
+      </main>
+      <Footer />              
     </div>
   );
 }
