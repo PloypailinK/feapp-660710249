1ce698aa174d9c3b418ca21daa2dfcc50429d663
PloypailinK <kwanwipithakran_p@su.ac.th>
2026-09-10 10:42:44 +0000

    new data and state

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { movies } from '../data';
 import MovieCard from '../components/MovieCard';
 import MovieGallery from '../components/MovieGallery';
