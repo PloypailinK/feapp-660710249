a9b4c6740d1190120ff67c8512f3d33f9b546171
PloypailinK <kwanwipithakran_p@su.ac.th>
2026-09-10 10:50:45 +0000

    new data and state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import About from './pages/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import MovieDetail from './pages/MovieDetail';  
-import NotFound from './pages/NotFound';  
+
 
 
 
